059feaab250c3294027b6358ccc75472fa4cc0b8
Uusi <julia@Uusi-iMac.fritz.box>
2016-01-25 18:03:52 +0200

    base window made. add button functionality made.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,12 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QDateTime date = QDateTime::currentDateTime();
+    visitorsToday.push_back(date);
+    ui->textBrowser->append(date.toString() + ": Visitor added");
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vector>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_2_clicked();
+
 private:
     Ui::MainWindow *ui;
+    std::vector<QDateTime> visitorsToday;
 };
 
 #endif // MAINWINDOW_H
